507f01e2cf28a6d3c1c754901494490f67ba8185
tatiana_vsh <tonik.germanskiy@gmail.com>
2025-05-17 12:04:11 +0200

    make build

---
+++ index.75126bd2.js
@@ -0,0 +1,2 @@
+!function e(n,t){if(Object.keys(t).length){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,Object.keys(t[a]).length>0&&e(l,t[a]),r.appendChild(l)}n.appendChild(r)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.75126bd2.js.map
